3f57335ce06288e921c8cf1fc2b237a4af29f0f3
jbiewer <jbiewer@wisc.edu>
2019-04-17 21:59:26 -0500

    Added p4_tests_json files (only from PackageManager tests)

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -29,6 +30,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Main.stage = primaryStage;
+        primaryStage.show();
         // hello, world!
     }
 

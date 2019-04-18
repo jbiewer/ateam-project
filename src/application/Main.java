97c0db2baa48a18b614534674444802df8d62d2c
jbiewer <jbiewer@wisc.edu>
2019-04-17 21:17:47 -0500

    Set up the basic methods/fields for Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,51 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The main class of the entire program. It's the Main Man.
+ */
 public class Main extends Application {
 
+    private static Stage stage; // the primary stage of the application
+
+    /**
+     * Method run before the application opens.
+     * Any initialization is done here.
+     * @throws Exception When there's a boo boo in the code.
+     */
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    /**
+     * Method run when the program executes.
+     * @param primaryStage Main stage for the application.
+     * @throws Exception When there's a boo boo in the code.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Main.stage = primaryStage;
         // hello, world!
     }
 
+    /**
+     * Good ol' PSVM
+     * @param args cmd line args.
+     */
     public static void main(String[] args) {
         launch(args);
     }
+
+    /**
+     * Used to switch between scenes on the primary stage.
+     * @param scene Scene to switch to.
+     */
+    public static void switchScene(Scene scene) {
+        Main.stage.setScene(scene);
+    }
+
 }

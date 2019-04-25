c76064ae99d3579354171e7fd62d14100eb01704
Jprazich <47827654+Jprazich@users.noreply.github.com>
2019-04-24 21:52:34 -0500

    Add files via upload

--- src/application/ui/root/ResultsRoot.java
+++ src/application/ui/root/ResultsRoot.java
@@ -1,13 +1,40 @@
 package application.ui.root;
 
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 // Parent to be changed to whatever layout we want to use later.
-public class ResultsRoot extends Parent {
-
+public class ResultsRoot extends VBox {
+	
+	private HBox middle;
+	
     public ResultsRoot() {
-        // create layout here.
-
+        // Initialize nodes
+    Label summary = new Label("Your results: ");
+    HBox percentageBox = new percentageBox();
+    HBox results = new resultsBox();
     }
-
+    
+    private class percentageBox extends HBox {
+    	private percentageBox() {
+    		//Get number of correct answers
+    		
+    		//int numQuestions = this.totalNumQuestions;
+    		
+    		//this.middle = new HBox(numCorrect, numQuestions, percentage);
+    		
+    		// Set style?
+    		
+    	}
+    }
+    
+    private class resultsBox extends HBox {
+    	private resultsBox() {
+    	// 
+    	//
+    	}
+    }
+    
 }
